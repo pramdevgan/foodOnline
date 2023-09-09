8c50b6e2d7aaf2dd4da4adfbdae75867ad6db624
pramdevgan <vpramatma@gmail.com>
2023-09-09 13:34:19 +0530

    vendor profile form + validation

--- accounts/models.py
+++ accounts/models.py
@@ -94,8 +94,7 @@ class UserProfile(models.Model):
     cover_photo = models.ImageField(
         upload_to="users/cover_photo", blank=True, null=True
     )
-    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
-    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=50, blank=True, null=True)
     country = models.CharField(max_length=20, blank=True, null=True)
     state = models.CharField(max_length=25, blank=True, null=True)
     city = models.CharField(max_length=25, blank=True, null=True)
@@ -104,9 +103,6 @@ class UserProfile(models.Model):
     longitude = models.CharField(max_length=20, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True)
-
-    def user_address(self):
-        return f"{self.address_line_1} {self.address_line_2}"
 
     def __str__(self):
         return self.user.email
---
+++ accounts/validators.py
@@ -0,0 +1,18 @@
+import os.path
+
+from django.core.exceptions import ValidationError
+
+
+def allow_only_images_validator(value):
+    ext = os.path.splitext(value.name)[1]  # cover-images.jpg ['cover-images', 'jpg]
+    print(ext)
+    valid_extensions = [
+        ".png",
+        ".jpg",
+        ".jpe",
+        ".jpeg",
+    ]
+    if not ext.lower() in valid_extensions:
+        raise ValidationError(
+            "Unsupported file Extension. Allowed Extension: " + str(valid_extensions)
+        )
